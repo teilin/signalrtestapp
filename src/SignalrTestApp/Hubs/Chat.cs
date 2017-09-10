6195dee51739adc9d636f8264c4e63613376332b
Teis Lindemark <teis@acos.no>
2017-09-10 22:20:38 +0200

    Init commit

---
+++ src/SignalrTestApp/Hubs/Chat.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalrTestApp.Hubs
+{
+    public class Chat : Hub
+    {
+        public override Task OnConnectedAsync()
+        {
+            return Clients.All.InvokeAsync("Send", $"{Context.ConnectionId} joined");
+        }
+
+        public override Task OnDisconnectedAsync(Exception exception)
+        {
+            return Clients.All.InvokeAsync("Send", $"{Context.ConnectionId} left");
+        }
+
+        public Task Send(string message)
+        {
+            return Clients.All.InvokeAsync("Send", message);
+        }
+    }
+}
